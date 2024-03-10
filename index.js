cd13134a69e967787f2f1f221e093f3e14037efd
XtryxVer <162854858+XtryxVer@users.noreply.github.com>
2024-03-10 00:50:47 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = [👑 I am RTX","Spotify","YouTube"];
 
 
 let currentIndex = 0;
